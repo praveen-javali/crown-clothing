a82007639efb2b01d419215f6b527a2f19de791f
Praveen Javali <praveen_j@BGMB128023.local>
2022-12-07 13:33:52 +0530

    add directory and category item component

---
+++ src/components/directory/Directory.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CategoryItem from "../catagory-items/CategoryItem.component";
+import "./Directory.component.scss";
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
+//rafce
